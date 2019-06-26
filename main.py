a5a5b8a36bb88b1796eddd249040a782362faeb3
ravmiecznik <nat132730>
2019-06-26 20:36:18 +0200

    disable objects before connection

--- main.py
+++ main.py
@@ -192,6 +192,7 @@ class MainWindow(QtGui.QMainWindow, BanksProcedures):
         Message.default_negative_signal = self.console_msg_factory("command failed")
 
         self.resize(x_siz, y_siz)
+        self.disable_objects_for_transmission()
         self.connect_button_slot()
 
     def initUI(self):
@@ -451,6 +452,7 @@ class MainWindow(QtGui.QMainWindow, BanksProcedures):
         self.connect_button.set_green_style_sheet()
         self.recevive_emulator_data_thread.start()
         self.get_bank_in_use()
+        self.enable_objects_after_transmission()
         #TODO tmp
         #self.reflash_button_slot()
 
