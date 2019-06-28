3b8b39bc83ba8ae88c8d84d3e4c561a98408e4d7
rafal <ravmiecznk@gmail.com>
2019-06-28 22:09:34 +0200

    sream read implementation step2

--- bin_handler/bin_handler.py
+++ bin_handler/bin_handler.py
@@ -7,11 +7,18 @@ import os
 from io import BytesIO
 from main_logger import error, warn, info, debug
 from message_handler.crc import crc
+import time, struct
 
 class BinSenderFileNotPresent(Exception):
     pass
 
 class BinSenderInvalidBinSize(Exception):
+    pass
+
+class PacketReceptionTimeout(Exception):
+    pass
+
+class CrcFail(Exception):
     pass
 
 #class BinSender(BytesIO):
@@ -67,3 +74,51 @@ class BinSender(file):
             hex_line = self.read(self.col_size)
             cnt += self.col_size
         return repr
+
+
+class BinReceiver(bytearray):
+    """
+    rx_buffer must have size at least 0x8000/SPMPAGESIZE/8
+    """
+    def __init__(self, rx_buffer, file_name, timeout=1):
+        bytearray.__init__(self)
+        self.__rx_buffer = rx_buffer
+        self.__file_name = file_name
+        self.__timeout = timeout
+        self.__expected_data_amount = 0x8000
+        self.__is_image_complete = False
+        self.__timeout = 1
+        self.__packet_size = 256 * 8 + 2 #plus CRC
+
+
+    def __iter__(self):
+        self.__init__(self.__rx_buffer, self.__file_name, self.__timeout)
+        print 100*'#'
+        print 10*'\n'
+        print 100 * '#'
+        return self
+
+
+    def next(self):
+        t0 = time.time()
+        while self.__rx_buffer.available() < self.__packet_size:
+            time.sleep(0.001)
+            if time.time() - t0 > self.__timeout:
+                raise PacketReceptionTimeout
+        data_received = self.__rx_buffer.read()
+        _crc = data_received[-2:]
+        data_received = data_received[0:-2]
+        print 'crc', _crc == crc(data_received)
+        self += data_received
+        if len(self) >= self.__expected_data_amount:
+            template = 4 * "{:02X} "
+            template = "{}  ".format(template)
+            template = 4 * template
+            index = 0
+            line = self[index: index + 16]
+            while line:
+                print template.format(*list(line)),
+                print '\t', line.replace('\n', ' ').replace('\r', ' ')
+                index += 16
+                line = self[index: index + 16]
+            raise StopIteration
