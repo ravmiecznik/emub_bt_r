25e117d9e2cc118d495317fe8e67683d6c46a482
rafal <ravmiecznk@gmail.com>
2019-07-08 18:31:39 +0200

    update, working version

--- bin_tracker/bin_tracker.py
+++ bin_tracker/bin_tracker.py
@@ -7,32 +7,57 @@ from bin_handler import bin_repr
 from my_gui_thread import GuiThread, thread_this_method
 from io import BytesIO
 import os
-
+import struct
+import time
 
 class BinFile(BytesIO):
     def __init__(self, *args, **kwargs):
         BytesIO.__init__(self, *args, **kwargs)
         self.seek(0)
 
-    def __repr__(self):
+    def __str__(self):
         return bin_repr(self)
 
+class DiffAddrByte(dict):
+    def popitem(self):
+        addr, byte = dict.popitem(self)
+        return struct.pack('H', addr) + byte
 
 class BinTracker(file):
 
-    def __init__(self, file_path):
+    def __init__(self, file_path, event_handler, blink_signal = lambda: None):
         file.__init__(self, file_path, 'rb')
         self.file_path = file_path
         self.current_file_state = BinFile(self.read())
         self.__mtime = os.path.getmtime(self.file_path)
-        self.track_file().start()
+        self.event_handler = event_handler
+        self.track_file()
+        self.diffs = DiffAddrByte()
+        self.pages_modified = set()
+        self.page_size = 256 * 8
+        self.blink_signal = blink_signal
 
+    def start(self):
+        self.track_file.start()
+
+    def stop(self):
+        self.track_file.terminate()
+
+    def __del__(self):
+        self.stop()
+
+    def resume(self):
+        self.track_file.resume()
 
     @thread_this_method(period=0.5)
     def track_file(self):
         if self.__mtime != os.path.getmtime(self.file_path):
             self.__mtime = os.path.getmtime(self.file_path)
             self.file_changed_procedure()
+        if self.diffs:
+            self.track_file.suspend()
+            self.event_handler.emulation_diffs_present_slot.emit()
+        self.blink_signal()
 
     def check_diffs(self):
         addr = 0
@@ -40,9 +65,10 @@ class BinTracker(file):
         self.current_file_state.seek(0)
         for a, b in zip(self.current_file_state.read(), self.read()):
             if a != b:
-                print "{:04X}: {:02X} -> {:02X} | {} - {}".format(addr, ord(a), ord(b), a, b)
+                self.diffs[addr] = b
+                page_modified = addr/self.page_size
+                self.pages_modified.add(page_modified)
             addr += 1
-        print
         self.seek(0)
         self.current_file_state = BinFile(self.read())
 
@@ -50,7 +76,7 @@ class BinTracker(file):
         self.check_diffs()
 
 
-    def __repr__(self):
+    def __str__(self):
         return bin_repr(self)
 
 
