7724e4316e1517c262393ccf91ea6b301a5c902f
rafal <ravmiecznk@gmail.com>
2019-08-16 15:27:17 +0200

    rename of BinSendter->BinFilePacketGenerator

--- bin_handler/__init__.py
+++ bin_handler/__init__.py
@@ -3,4 +3,4 @@ author: Rafal Miecznik
 contact: ravmiecznk@gmail.com
 """
 
-from bin_handler import BinSender, BinSenderFileNotPresent, BinSenderInvalidBinSize, BinReceiver, ReceptionFail, PacketReceptionTimeout, bin_repr
+from bin_handler import BinFilePacketGenerator, BinSenderFileNotPresent, BinSenderInvalidBinSize, BinReceiver, ReceptionFail, PacketReceptionTimeout, bin_repr
