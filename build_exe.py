3c0e05e2b074c6089080f9952f3071d3518736e8
ravmiecznik <ravmiecznk@gmail.com>
2019-11-23 14:10:51 +0100

    exe file name after operating system

--- build_exe.py
+++ build_exe.py
@@ -1,9 +1,18 @@
 import PyInstaller.__main__
 import main
-import os
+import os, platform, sys
+
+system_name = platform.system() + platform.release()
 
 icon_path = os.path.join('spec', 'icon.ico')
-PyInstaller.__main__.run(['--name=emubt', '-F', '--windowed', '-i={}'.format(icon_path),
+
+#console
+# PyInstaller.__main__.run(['--name=emubt', '-F', '-i={}'.format(icon_path),
+#                           '--specpath=spec', '--onefile',
+#                           'main_release.py'])
+
+#no console
+PyInstaller.__main__.run(['--name=emubt_{}'.format(system_name), '-F', '--windowed', '-i={}'.format(icon_path),
                           '--specpath=spec', '--onefile',
                           'main_release.py'])
 
