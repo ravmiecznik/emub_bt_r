65b52a17bf28da9c5c8c4a804c7f6f9a49bebdf5
rafal <ravmiecznk@gmail.com>
2019-11-24 20:32:14 +0100

    removed print

--- digidiag/digidiag_window.py
+++ digidiag/digidiag_window.py
@@ -107,7 +107,7 @@ class DigidiagWindow(QtGui.QWidget):
                 raw_value = self.__ord_to_int_vs_size[byte_size](raw_value)[0]
                 self.values_editor.display_value(value, raw_value, byte_size)
             except (KeyError, struct.error) as e:
-                print e.message
+                #print e.message
                 #traceback.print_exc()
                 pass
 
