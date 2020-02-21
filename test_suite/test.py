9c0ec63d22d24fd70232b024fd8d3e7ebf506744
rafal <ravmiecznk@gmail.com>
2020-02-21 20:30:13 +0100

    ignoring all DBG DOWNLOADED directories

--- test_suite/test.py
+++ test_suite/test.py
@@ -2,9 +2,9 @@
 author: Rafal Miecznik
 contact: ravmiecznk@gmail.com
 """
-
-import unittest
 import sys
+sys.path.append('../')
+import unittest
 from PyQt4.QtGui import QApplication, QWidget
 from PyQt4.QtCore import pyqtSignal, QEvent
 import threading
@@ -13,11 +13,13 @@ import time
 import inspect
 import os
 import configparser
+from event_handler import to_signal
 from gui_thread import GuiThread
 
 DOWNLOADED = 'DOWNLOADED'
 APP_STATUS_FILE = 'app_status.sts'
 FILE_LIST_TAG = "LAST BIN FILES"
+RESOURCE = 'RESOURCE'
 
 def clean_downloaded_files():
     files = os.listdir(DOWNLOADED)
@@ -43,34 +45,26 @@ def clean_app_status_files_list():
     with open(APP_STATUS_FILE, 'w') as cf:
         config.write(cf)
 
+
+def wait_for(event, timeout=5, period=0.1):
+    """
+    callable even
+    :param event: callable, maybe expression in lambda or any callable which returns True/Flase
+    :param timeout:
+    :return:
+    """
+    t0 = time.time()
+    while time.time() - t0 < timeout:
+        if event():
+            return
+        time.sleep(period)
+    try:
+        msg = "Timeout {} for event: {}".format(timeout, inspect.getsource(event))
+    except TypeError:
+        msg = "Timeout {} for event: {}".format(timeout, event.__name__)
+    assert False, msg
+
 class TestQApplication(unittest.TestCase):
-
-    def wait_for(self, event, timeout=5, period=0.1):
-        """
-        callable even
-        :param event: callable, maybe expression in lambda or any callable which returns True/Flase
-        :param timeout:
-        :return:
-        """
-        t0 = time.time()
-        while time.time() - t0 < timeout:
-            if event():
-                return
-            time.sleep(period)
-        try:
-            msg = "Timeout {} for event: {}".format(timeout, inspect.getsource(event))
-        except TypeError:
-            msg = "Timeout {} for event: {}".format(timeout, event.__name__)
-        assert False, msg
-
-    def run(self, *args, **kwargs):
-        t0 = time.time()
-        max_timeout = 10
-        while self.main_window.is_connected() != "disconnect":
-            time.sleep(0.5)
-            if time.time() - t0 > max_timeout:
-                self.fail("Connection timeout")
-        unittest.TestCase.run(self, *args, **kwargs)
 
     def test_if_connected(self):
         """
@@ -79,23 +73,50 @@ class TestQApplication(unittest.TestCase):
         """
         assert self.main_window.is_connected() == "disconnect"
 
-    def test_read_sram(self):
-        # TODO:
-        #upload file
-        self.main_window.bin_file_panel.combo_box.clear()
-        self.main_window.bin_file_panel.combo_box.clearEditText()
-        self.main_window.read_sram_button_slot()
-        self.wait_for(
-            lambda :os.path.isfile(str(self.main_window.bin_file_panel.combo_box.currentText())),
-            timeout=10, period=1
-        )
-        #TODO:
-        #compare file
+    def test_if_sram_zero(self):
+        to_signal(self.main_window.bin_file_panel.combo_box.clear)()
+        to_signal(self.main_window.bin_file_panel.combo_box.clearEditText)()
+        to_signal(self.main_window.read_sram_button_slot)()
+        self.main_window.is_downloaded_file_present()
+        self.main_window.get_current_file()
+        file_path = self.main_window.get_current_file.qget()
+        addr = 0
+        with open(file_path) as f:
+            chars = f.read()
+            for c in chars:
+                self.assertEqual(c, '\x00', "Sram file not zero at addr: {}".format(addr))
+                addr += 0
+
+    # def test_upload_bank(self):
+    #     new_file = os.path.join(RESOURCE, '8VG60.bin')
+    #     self.queue.put({'file_to_upload': new_file})
+    #     to_signal(self.main_window.set_new_file_for_upload)()
+    #     to_signal(self.main_window.save_button_slot)()
+    #     self.wait_for_queue_event({'text_browser': "File transmitted in:"},
+    #                               to_signal(self.main_window.get_text_browser_to_queue), timeout=15)
+
+
+
 
     @classmethod
     def setUpClass(cls):
         clean_downloaded_files()
         clean_app_status_files_list()
+
+        connection_timeout = 10
+        t0 = time.time()
+        while cls.main_window.is_connected() != "disconnect":
+            time.sleep(0.5)
+            assert time.time() - t0 < connection_timeout, "Connection timeout"
+
+        # cls.main_window.wipe_banks()
+        # wait_for(lambda : "wiping done" in str(cls.main_window.console.console_text_browser.toPlainText()), period=1)
+
+        cls.main_window.bank1set_slot()
+        time.sleep(0.5)
+        cls.main_window.get_active_bank_button()
+        cls.queue = cls.main_window.queue
+        wait_for(lambda: cls.main_window.get_active_bank_button() == 1)
 
     @classmethod
     def tearDownClass(cls):
@@ -103,7 +124,8 @@ class TestQApplication(unittest.TestCase):
         while cls.main_window.is_connected() != "Connect":
             time.sleep(0.5)
         time.sleep(1)
-        cls.main_window.close()
+        to_signal(cls.main_window.close)()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -120,4 +142,5 @@ if __name__ == "__main__":
     thread = threading.Thread(target=unittest.main, kwargs={'verbosity': 2})
     thread.start()
     app.exec_()
+    thread.join()
     sys.exit()
