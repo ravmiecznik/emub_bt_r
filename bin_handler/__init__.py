492089bb476eb8a445ae7c9f4011584e7d772458
rafal <ravmiecznk@gmail.com>
2020-03-30 20:44:30 +0200

    freemem module added

--- bin_handler/__init__.py
+++ bin_handler/__init__.py
@@ -3,4 +3,5 @@ author: Rafal Miecznik
 contact: ravmiecznk@gmail.com
 """
 
-from bin_handler import BinFilePacketGenerator, BinSenderFileNotPresent, BinSenderInvalidBinSize, BinReceiver, ReceptionFail, PacketReceptionTimeout, bin_repr
+from bin_handler import BinFilePacketGenerator, BinSenderFileNotPresent, BinSenderInvalidBinSize, ReceptionFail, \
+    PacketReceptionTimeout, bin_repr, BinReceiver
