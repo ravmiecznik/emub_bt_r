e0a638638207a2b06992532c508f196e805342b8
rafal <ravmiecznk@gmail.com>
2020-04-05 12:22:08 +0200

    adapted for adjustable packetsize, dynamic calculation of tx max timeout based on num of packets and retx_tiemeout

--- procedures.py
+++ procedures.py
@@ -72,8 +72,8 @@ class WritePackets:
 
     def send_packet(self, packet, b_address):
         msg_body = struct.pack('H', b_address) + packet
-        print bin_repr(BytesIO(packet))
-        print "=========================================\n"
+        # print bin_repr(BytesIO(packet))
+        # print "=========================================\n"
         _context = self.message_sender.send(MessageSender.ID.write_to_page, body=msg_body)
         return _context
 
@@ -82,7 +82,7 @@ class WritePackets:
         to_signal(self.progress_bar.hide).emit()
         self.enable_objects_after_transmission_signal.emit()
 
-    def write_packets_procedure(self):
+    def send_rx_flush_req(self):
         retx = 3
         def rx_flush():
             context = self.message_sender.send(m_id=MessageSender.ID.rxflush)
@@ -92,6 +92,7 @@ class WritePackets:
                     return
                 time.sleep(0.1)
             return True
+
         while retx:
             if rx_flush():
                 break
@@ -100,39 +101,40 @@ class WritePackets:
                 self.gui_communication_signal.emit("RX flush failed")
                 return
 
+    def write_packets_procedure(self):
+        self.send_rx_flush_req()
         self.disable_objects_for_transmission_signal.emit()
         time.sleep(0.5)
-        max_timeout = 60
         t_start = time.time()
         bank_name_full = os.path.basename(self.bin_packets.bin_path)
         bank_name = os.path.splitext(bank_name_full)[0]
         self.progress_bar.set_title("SENDING: {}".format(bank_name_full))
         to_signal(self.progress_bar.display).emit()
-
         tot_tx_bytes = 0
-        packet = self.bin_packets.next()
-        debug("Sending bin with packetsize: {}".format(len(packet.payload)))
-        print "packets", self.bin_packets.packets_amount
         while self.progress_bar.isHidden(): time.sleep(0.1)
-        while tot_tx_bytes < len(self.bin_packets):
-            print "packetsize", len(packet.payload)
+        packets = {p.b_address: p for p in self.bin_packets}
+        packet = packets.get(packets.keys()[0])
+        max_timeout = len(packets) * self.__retx_timeout * 4
+        print "max timeout", max_timeout/60
+        debug("Sending bin with packetsize: {}".format(len(packet.payload)))
+        while packets:
             if self.progress_bar.isHidden():
                 self.gui_communication_signal.emit("Upload procedure terminated")
                 break
             self.check_resp_thr = GuiThread(self.check_repsonse, args=(MessageSender.context,))
-            print "context", MessageSender.context
             self.check_resp_thr.start()
-            self.send_packet(packet.payload, b_address=tot_tx_bytes)
+            self.send_packet(packet.payload, b_address=packet.b_address)
             while self.check_resp_thr.returned() is None: time.sleep(0.001)
             response = self.check_resp_thr.returned()
 
             if response == RxMessage.rx_id_tuple.index('ack'):
                 self.tx_stats.ack()
                 self.progress_bar.set_val_signal.emit(float(tot_tx_bytes) / self.bin_packets.packets_amount * 100)
-                tot_tx_bytes += packet.b_address
+                tot_tx_bytes += len(packet.payload)
+                packets.pop(packet.b_address)
                 try:
-                    packet = self.bin_packets.next()
-                except StopIteration:
+                    packet = packets.get(packets.keys()[0])
+                except IndexError:
                     pass
             elif response == RxMessage.rx_id_tuple.index('dtx'):
                 self.tx_stats.dtx()
@@ -162,7 +164,6 @@ class ReadPackets:
         self.progress_bar = parent.progress_bar
         self.tx_stats = TransmissionStats()
         self.read_thread = GuiThread(self.read_packets_procedure)
-        #self.received = BytesIO()
         self.context_to_packet_map = {}
         self.receiver = BinReceiver(packet_size=256*8)
         self.get_bank_name = parent.banks_panel.get_bank_name_text
@@ -188,7 +189,6 @@ class ReadPackets:
                 packet_num = self.context_to_packet_map[context]
                 self.context_to_packet_map.pop(context)
                 self.receiver[packet_num] = msg.msg
-                #self.received.write(msg.msg)
                 result = msg.id
                 return result
 
@@ -212,7 +212,6 @@ class ReadPackets:
         to_signal(self.progress_bar.display).emit()
         packet_num = 0
         while self.progress_bar.isHidden(): time.sleep(0.1)
-        #while packet_num < 16:
         while not self.receiver:
             if self.progress_bar.isHidden():
                 self.gui_communication_signal.emit("Read procedure terminated")
